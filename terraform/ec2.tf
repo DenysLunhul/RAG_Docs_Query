4339f9b6201d1a01562c5c03e8a60d8c983597a4
Denys Lunhul <lundenis15@gmail.com>
2026-05-29 18:52:53 +0300

    attached IAM role to ec2 instance through terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "knowledge_base" {
   ami           = "ami-05cf1e9f73fbad2e2"
   instance_type = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   vpc_security_group_ids = [
     data.aws_security_group.ec2.id,
